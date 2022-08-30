4cb9a077d0c1703cc530bb1110545db3f5e8cf40
DarkLord239 <husseinib23920@gmail.com>
2022-08-30 17:38:54 +0300

    Create the alert message

--- assets/screens/StartGameScreen.js
+++ assets/screens/StartGameScreen.js
@@ -1,20 +1,49 @@
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Alert } from 'react-native';
 import PrimaryButton from '../../components/PrimaryButton';
 
 const StartGameScreen = () => {
+    const [enteredNumber, setEnteredNumber] = useState('');
 
+    function numberInputHandler(enteredText) {
+        setEnteredNumber(enteredText)
+    }
+    function resetInputHandler() {
+        setEnteredNumber('')
+    }
+
+    function confirmNumberHandler() {
+        const number = parseInt(enteredNumber)
+        if (isNaN(number) || number <= 0 || number > 99) {
+            Alert.alert("Invalid Number!",
+                "Number has to be a number between 1 and 99.",
+                [
+                    {
+                        text: "Cancel",
+                        onPress: () => resetInputHandler(),
+                        style: "cancel",
+                    },
+                ],
+            )
+            return;
+        }
+    }
     return (
         <View style={styles.inputContainer}>
-            <TextInput style={styles.textInputContainer} maxLength={2} keyboardType='number-pad' />
+            <TextInput
+                style={styles.textInputContainer}
+                maxLength={2}
+                keyboardType='number-pad'
+                onChangeText={numberInputHandler}
+                value={enteredNumber}
+            />
             <View style={styles.buttonsContainer}>
                 <PrimaryButton>Reset</PrimaryButton>
-                <PrimaryButton>Confirm</PrimaryButton>
+                <PrimaryButton onPressing={confirmNumberHandler}>Confirm</PrimaryButton>
             </View>
         </View>
     )
-
 }
-
 const styles = StyleSheet.create({
     inputContainer: {
         padding: 16,
@@ -28,15 +57,15 @@ const styles = StyleSheet.create({
         shadowRadius: 6,
         shadowOpacity: 1,
         shadowOffset: { width: 0, height: 6 },
-        marginTop:150,
+        marginTop: 150,
 
     },
     buttonsContainer: {
         flexDirection: 'row',
     },
-    textInputContainer:{
-        height:50,
-        width:50,
+    textInputContainer: {
+        height: 50,
+        width: 50,
         borderBottomColor: 'yellow',
         borderBottomWidth: 2,
         marginVertical: 10,
@@ -47,9 +76,9 @@ const styles = StyleSheet.create({
         color: '#ffffff',
         alignItems: 'center'
     },
-    buttonContainer:{
-        flex:1,
-    }    
+    buttonContainer: {
+        flex: 1,
+    }
 })
 
 export default StartGameScreen;
--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,14 +1,13 @@
 import { Children } from "react";
 import { View, Text, Pressable, StyleSheet } from "react-native";
 
-function PrimaryButton({ children }) {
-    function onPressing() {
-        console.log('pressed!')
-    }
-
+function PrimaryButton({ children, onPressing }) {
     return (
         <View style={styles.buttonOuterContainer}>
-            <Pressable style={styles.buttonInnerContainer} onPress={onPressing} android_ripple={{ color: '#490202' }}>
+            <Pressable
+                style={styles.buttonInnerContainer}
+                onPress={onPressing}
+                android_ripple={{ color: '#490202' }}>
                 <Text style={styles.buttonText}>
                     {children}
                 </Text>
@@ -20,20 +19,20 @@ function PrimaryButton({ children }) {
 
 const styles = StyleSheet.create({
     buttonOuterContainer: {
-        flex:1,
+        flex: 1,
         margin: 10,
-        borderRadius:28,
+        borderRadius: 28,
         overflow: 'hidden',
     },
     buttonInnerContainer: {
         backgroundColor: '#a40248',
-        paddingVertical: 10,     
+        paddingVertical: 10,
     },
     buttonText: {
         color: '#f6f6f6',
         textAlign: 'center'
     },
-    
+
 })
 
 export default PrimaryButton;
